cb992e40dee29dae41d42b45e3ec387924caeb72
Prasad Poul <prasadpoul.2024.com>
2024-08-18 11:31:29 +0530

    first commit

---
+++ Assignment4p1/Assignment4p1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment4p1
+{
+    internal class Product
+    {
+        int pCode;
+        public string pName;
+        public int qtyInStock;
+        int discounAllowd;
+        static brand;
+        public int price;
+        pubic int amount;
+    }
+
+    public void GetDetails()
+    {
+        Console.WriteLine("pCode"
+    }
+}
